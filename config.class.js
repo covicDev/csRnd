95348ae9420ebda508ad3eb87b2b40013b6f16d2
covicDev <covicdev@gmail.com>
2021-03-24 13:31:39 +0100

    add config file

---
+++ config.class.js
@@ -0,0 +1,39 @@
+class _Config {
+    constructor(){
+        /* 
+            Mapy: 
+            przez zastosowanie komentarza '//' albo
+            przez wpisanie '0' w trzecią kolumnę,
+            można wyłączyć z listy losowania.
+        */
+        this.mapsArray = [
+
+            ["DUST II",     172,     1],
+            ["INFERNO",     342,     1],
+            ["MIRAGE",      512,     1],
+            ["CACHE",       682,     1],
+            ["COBBLE",      852,     1],
+            ["OVERPASS",    1020,    1],
+            ["TRAIN",       1188,    1],
+            ["NUKE",        1358,    1],
+            ["VERTIGO",     1528,    1],
+            ["CANALS",      1698,    1],
+            ["ITALY",       1868,    1],
+            ["OFFICE",      2033,    1],
+            ["ANUBIS",      2203,    1],
+            ["DUST",        2373,    1],
+            ["BANK",        2543,    1],
+            ["LAKE",        2713,    1],
+            ["S.HOUSE",     2881,    1],
+            ["MILITIA",     3050,    0],
+        //  ["NewMap",      numb,    1],
+
+        ];
+    }
+    // get
+    getMapsArray(){
+        return this.mapsArray;
+    }
+}
+
+
